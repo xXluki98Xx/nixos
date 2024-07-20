016cb2e8c99f3d3352c73998f13aa6a4247311e2
lRamm <lramm.dev@gmail.com>
2024-07-20 21:26:24 +0200

    fix(shell): add keybinds

--- users/lramm/shell/zsh.nix
+++ users/lramm/shell/zsh.nix
@@ -13,6 +13,31 @@
 
     # Definiere benutzerdefinierte Funktionen
     initExtra = ''
+      zstyle ':autocomplete:*' fzf-completion yes
+
+      # Insert unambigous prefix first before completing
+      # all Tab widgets
+      zstyle ':autocomplete:*complete*:*' insert-unambiguous yes
+      # all history widgets
+      zstyle ':autocomplete:*history*:*' insert-unambiguous yes
+      # ^S
+      zstyle ':autocomplete:menu-search:*' insert-unambiguous yes
+
+      # include "." and ".." in completion output
+      zstyle ':completion:*:paths' special-dirs true
+
+      # Fix pos1, end etc. for terminal emulator
+      bindkey  "^[[H"   beginning-of-line #pos1
+      bindkey  "^[[F"   end-of-line #end
+
+      bindkey "^[[1;5C" forward-word # Ctrl-RArrow
+      bindkey "^[[1;5D" backward-word # Ctrl-LArrow
+
+      bindkey -s "^[[5~" '^[[A' # Page Up
+      bindkey -s "^[[6~" '^[[B' # Page Down
+
+      bindkey  "^[[3~"  delete-char #del
+
       # change dir and create if not exist
       mkcd() { local dir="$1"; mkdir --parents "$dir" && cd "$dir"; }
     '';
