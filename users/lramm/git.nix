df529ddf5a2a77b6e476268d770b18df3af75ff0
lRamm <lramm.dev@gmail.com>
2024-07-20 15:50:55 +0200

    feat(git): add shell aliases

--- users/lramm/git.nix
+++ users/lramm/git.nix
@@ -7,15 +7,25 @@
       add = "git add";
       s = "git s";
       cm = "git cm";
+      amend = "git amend";
+      reword = "git reword";
+      undo = "git undo";
+      rh = "git rh";
     };
 
     # Git Settings
     git = {
       enable = true;
+
       aliases = {
         s = "status";
         cm = "commit --message";
+        amend  = "commit --amend --no-edit";
+        reword = "commit --amend --message";
+        undo = "reset HEAD~1";
+        rh = "reset --hard";
       };
+
       extraConfig = {
         user = {
           email = "lramm.dev@gmail.com";
